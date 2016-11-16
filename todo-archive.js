24b1328a18bbf9f9f15d90ec565b27c8593e8ac3
hbaughman <hbaughman741@gmail.com>
2016-11-16 18:49:52 -0500

    Read and parse yaml files

--- todo-archive.js
+++ todo-archive.js
@@ -2,15 +2,29 @@
 "use strict";
 
 const program = require("commander")
+const fsp = require("fs-promise")
+const path = require("path")
+const yaml = require("js-yaml")
 
 program
   .option("-k, --keep", "Keep empty projects and/or contexts")
   .parse(process.argv)
 
-const [todoPath, donePath] = program.args
+let [todoPath, donePath = `${path.dirname(todoPath)}/done.yml`] = program.args
 
-console.log("program.keep: ", program.keep)
-console.log("program.args: ", program.args)
+Promise.all([
+  fsp.readFile(todoPath, "utf-8"),
+  fsp.readFile(donePath, "utf-8").catch(err =>
+    console.log("error with donePath", err
+  )),
+]).then(files => {
+  const json = files.map(yaml.safeLoad)
+  console.log("json: ", json)
+}).catch(err => {
+  console.error(`Could not read ${todoPath}`)
+  throw err
+})
 
-console.log("outside todo: ", todoPath)
-console.log("outside done: ", donePath)
+// console.log("program.keep: ", program.keep)
+// console.log("program.args: ", program.args)
+
