9df8795b1aaee0862e916a5b10cbb60c8a3faf09
hbaughman <hbaughman741@gmail.com>
2016-12-05 18:15:36 -0500

    removeNulls works on deeply nested values

--- src/removeNulls.js
+++ src/removeNulls.js
@@ -1,36 +1,5 @@
 const R = require("ramda")
 
-// R.pipe(
-//   yaml.safeLoad,
-//   R.filter(R.pipe(R.isNil, R.not)),
-//   R.map(removeNulls)
-// )
-
-// const removeNulls = (item) => {
-//   if (isFunctor(item)) {
-//     return R.pipe(
-//       shallowRemoveNulls
-//     )
-//     item = shallowRemoveNulls(item)
-//     return item.map(removeNulls)
-//   }
-//   else {
-//     return item
-//   }
-// }
-
-
-// function removeNulls(input) {
-//   const containsCompleted = R.test(regex.completed)
-//   if (containsCompleted(input)) {
-//     throw new TypeError("Must not contain completed todos (yaml commments)")
-//   }
-
-//   const todos = yaml.safeLoad(input)
-//   return input
-// }
-
-
 const isFunctor = R.either(R.is(Array), R.is(Object))
 const shallowRemoveNulls = R.filter(R.pipe(R.isNil, R.not))
 
--- src/removeNulls.test.js
+++ src/removeNulls.test.js
@@ -3,12 +3,17 @@ const removeNulls = require("./removeNulls")
 
 test("removeNulls should return the input if no props are null", t => {
   const input = { groceryStore: ["milk"], hardwareStore: ["nails"] }
-  const output = removeNulls(input)
   t.deepEqual(removeNulls(input), input)
 })
 
-test("removeNulls should remove nulls", t => {
+test("removeNulls should remove shallow nulls", t => {
   const input = { groceryStore: null, hardwareStore: ["nails"] }
   const output = { hardwareStore: ["nails"] }
+  t.deepEqual(removeNulls(input), output)
+})
+
+test("removeNulls should remove deep nulls", t => {
+  const input = { groceryStore: { cake: "sugar", dinner: null } }
+  const output = { groceryStore: { cake: "sugar" } }
   t.deepEqual(removeNulls(input), output)
 })
