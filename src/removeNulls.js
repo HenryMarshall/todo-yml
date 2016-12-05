3d5116ffeb762a84171f280338ce43c0cae3ea64
hbaughman <hbaughman741@gmail.com>
2016-12-05 17:54:17 -0500

    removeNulls processes json not yaml

--- src/removeNulls.js
+++ src/removeNulls.js
@@ -1,13 +1,48 @@
 const R = require("ramda")
-const yaml = require("js-yaml")
-const { regex } = require("./utility")
 
-function removeNulls(input) {
-  const containsCompleted = R.test(regex.completed)
-  if (containsCompleted(input)) {
-    throw new TypeError("Must not contain completed todos (yaml commments)")
-  }
-  return input
+// R.pipe(
+//   yaml.safeLoad,
+//   R.filter(R.pipe(R.isNil, R.not)),
+//   R.map(removeNulls)
+// )
+
+// const removeNulls = (item) => {
+//   if (isFunctor(item)) {
+//     return R.pipe(
+//       shallowRemoveNulls
+//     )
+//     item = shallowRemoveNulls(item)
+//     return item.map(removeNulls)
+//   }
+//   else {
+//     return item
+//   }
+// }
+
+
+// function removeNulls(input) {
+//   const containsCompleted = R.test(regex.completed)
+//   if (containsCompleted(input)) {
+//     throw new TypeError("Must not contain completed todos (yaml commments)")
+//   }
+
+//   const todos = yaml.safeLoad(input)
+//   return input
+// }
+
+
+const isFunctor = R.either(R.is(Array), R.is(Object))
+const shallowRemoveNulls = R.filter(R.pipe(R.isNil, R.not))
+
+function removeNulls(obj) {
+  return R.ifElse(
+    isFunctor,
+    R.pipe(
+      shallowRemoveNulls,
+      R.map(removeNulls)
+    ),
+    R.identity
+  )(obj)
 }
 
 module.exports = removeNulls
--- src/removeNulls.test.js
+++ src/removeNulls.test.js
@@ -1,18 +1,14 @@
 const test = require("ava")
 const removeNulls = require("./removeNulls")
 
-test("removeNulls should return the contents if none have null", t => {
-  const input =
-`groceryStore:
-  - milk
-hardwareStore:
-  - nails`
-  t.is(removeNulls(input), input)
+test("removeNulls should return the input if no props are null", t => {
+  const input = { groceryStore: ["milk"], hardwareStore: ["nails"] }
+  const output = removeNulls(input)
+  t.deepEqual(removeNulls(input), input)
 })
 
-test("removeNulls should blow up if yaml includes completed", t => {
-  const input =
-`groceryStore:
-  # milk`
-  t.throws(() => removeNulls(input))
+test("removeNulls should remove nulls", t => {
+  const input = { groceryStore: null, hardwareStore: ["nails"] }
+  const output = { hardwareStore: ["nails"] }
+  t.deepEqual(removeNulls(input), output)
 })
