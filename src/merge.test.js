6a4edb0828d510945dbadfa26977e2b4211202ac
hbaughman <hbaughman741@gmail.com>
2016-12-06 13:17:23 -0500

    Improve merge test coverage

--- src/merge.test.js
+++ src/merge.test.js
@@ -2,30 +2,46 @@ const test = require("ava")
 const merge = require("./merge")
 
 test("should have no effect if nothing is completed", t => {
-  const activeYaml = {}
-  const archivedYaml = { groceryStore: ["bananas"] }
-  const newArchive = merge(archivedYaml, activeYaml)
-  t.deepEqual(archivedYaml, newArchive)
+  const completed = {}
+  const archived = { groceryStore: ["bananas"] }
+  const newArchived = merge(archived, completed)
+  t.deepEqual(newArchived, archived)
+})
+
+test("should copy completed if archive is empty", t => {
+  const completed = { groceryStore: ["bananas"] }
+  const archived = {}
+  const newArchived = merge(archived, completed)
+  t.deepEqual(newArchived, completed)
 })
 
 test("should move todos from hitherto unseen projects", t => {
-  const newArchive = merge(
+  const newArchived = merge(
     { groceryStore: ["bananas"] },
     { hardwareStore: ["nails"] }
   )
-  const expectedArchive = {
+  const expectedArchived = {
     groceryStore: ["bananas"],
     hardwareStore: ["nails"],
   }
-  t.deepEqual(newArchive, expectedArchive)
+  t.deepEqual(newArchived, expectedArchived)
 })
 
 test("should concat completed todos from the same project", t => {
-  const newArchive = merge(
+  const newArchived = merge(
     { groceryStore: ["bananas"] },
     { groceryStore: ["apples"] }
   )
-  const expectedArchive = { groceryStore: ["bananas", "apples"] }
-  t.deepEqual(newArchive, expectedArchive)
+  const expectedArchived = { groceryStore: ["bananas", "apples"] }
+  t.deepEqual(newArchived, expectedArchived)
 })
 
+test("should throw if mixing todos and keys", t => {
+  const archivedObj = { groceryStore: { cookies: "sugar" } }
+  const completedArr = { groceryStore: ["bananas"] }
+  t.throws(merge.bind(null, archivedObj, completedArr), TypeError)
+
+  const archivedArr = { groceryStore: ["sugar"] }
+  const completedObj = { groceryStore: { parfait: "bananas" } }
+  t.throws(merge.bind(null, archivedArr, completedObj), TypeError)
+})
